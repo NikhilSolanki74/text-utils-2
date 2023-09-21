6a719591b53eee12a88ed2f906f52e5bf099a530
Nikhil <solankinikhil74@gmail.com>
2023-09-21 16:59:41 +0530

    improve security

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,64 +1,69 @@
-import React from 'react'
+import React from 'react';
 
 export default function Navbar(props) {
   return (
     <>
-    <nav className={`navbar navbar-expand-lg bg-${props.darkMode} `}>
-  <div className="container-fluid ">
-    <a className={`navbar-brand text-${props.darkMode === 'dark' ? 'white' : 'dark'} `} href="/">{props.title}</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className={`nav-link active text-${props.darkMode === 'dark' ? 'white' : 'dark'}`} aria-current="page" href="/">Home</a>
-        </li>
-        {/* <li className="nav-item">
-          <a className="nav-link text-white" href="/">Link</a>
-        </li> */}
-        {/* <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle text-white" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+      <nav className={`navbar navbar-expand-lg bg-${props.darkMode}`}>
+        <div className="container-fluid">
+          <a
+            className={`navbar-brand text-${props.darkMode === 'dark' ? 'white' : 'dark'}`}
+            href="https://text-utill-react.onrender.com"
+            // Make sure to sanitize external URLs
+          >
+            {props.title}
           </a>
-          <ul className="dropdown-menu text-white">
-            <li><a className="dropdown-item " href="/">Action</a></li>
-            <li><a className="dropdown-item" href="/">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="/">Something else here</a></li>
-          </ul>
-        </li> */}
-        <li className="nav-item">
-          <a className={`nav-link disabled  text-${props.darkMode === 'dark' ? 'white' : 'dark'}`} aria-disabled="true" href='/'>{props.quot}</a>
-        </li>
-      </ul>
-      {/* <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form> */}
-    </div>
-    {/* <div className="custom-control custom-switch">
-  <input type="checkbox" className="custom-control-input" disabled id="customSwitch2"/>
-  <label className="custom-control-label" for="customSwitch2">Disabled switch element</label>
-</div> */}
-    
-    <div className="btn-group" role="group" aria-label="Basic example">
-  {/* <button type="radio" onClick={props.toggleMode} className="btn btn-success">Light</button> */}
-  <button type="radio" onClick={props.toggleMode} className="btn btn-success">Dark Mode</button>
-</div>
-{/* <div class= "custom-control custom-switch ">
-  <input type="checkbox" class="custom-control-input " onClick={props.toggleMode} id="customSwitch1"/>
-  <label className="custom-control-label text text-danger" htmlFor="customSwitch1">Dark Mode</label>
-</div> */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="/navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a
+                  className={`nav-link active text-${props.darkMode === 'dark' ? 'white' : 'dark'}`}
+                  aria-current="page"
+                  href="/"
+                  // Ensure any dynamic data in the href is properly sanitized
+                >
+                  Home
+                </a>
+              </li>
+              {/* ... Other nav items */}
+              <li className="nav-item">
+                <a
+                  className={`nav-link disabled  text-${props.darkMode === 'dark' ? 'white' : 'dark'}`}
+                  aria-disabled="true"
+                  href="/"
+                  // Ensure any dynamic data in the href is properly sanitized
+                >
+                  {props.quot}
+                </a>
+              </li>
+            </ul>
+          </div>
 
-    </div>
-    
-  
-</nav>
-</>
-  )
+          <div className="btn-group" role="group" aria-label="Basic example">
+            <button
+              type="button"
+              onClick={props.toggleMode}
+              className="btn btn-success"
+            >
+              Dark Mode
+            </button>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
- 
-Navbar.defaultProps ={
-    quot: '---------->'
+
+Navbar.defaultProps = {
+  quot: '---------->',
 };
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,59 +1,62 @@
-import React, {useState} from 'react'
-
+import React, { useState } from 'react';
+import sanitizeHtml from 'sanitize-html';
 
 export default function SearchBox(props) {
-  
-  const handleUpClick =() =>{
-    // console.log("Uppercase was clicked" + text);
-    let newText = text.toUpperCase();
+  const [text, setText] = useState('');
+
+  const handleUpClick = () => {
+    const newText = text.toUpperCase();
     setText(newText);
   }
 
-  const handleOnChange = (event)=>{
-    // console.log("on change");
-    setText(event.target.value)
+  const handleOnChange = (event) => {
+    setText(event.target.value);
   }
 
-  const lowerCase =()=>{
-    let k=text.toLowerCase();
+  const lowerCase = () => {
+    const k = text.toLowerCase();
     setText(k);
   }
 
-  const emt=()=>{
-    setText('')
+  const emt = () => {
+    setText('');
   }
 
-  const nks=()=>{
-    let p=text.trim().split(/\s+/).join(" ");
+  const nks = () => {
+    const p = text.trim().split(/\s+/).join(" ");
     setText(p);
   }
 
-  const [text , setText] = useState('');
-  
+  const sanitizedText = sanitizeHtml(text, {
+    allowedTags: [],
+    allowedAttributes: {}
+  });
+
   return (
-    <div class={` bg-${props.darkMode} text-${props.darkMode === 'dark' ? 'white' : 'dark'}  rounded  pr-3 pb-3 pl-3 `}>
-  <div className='container'>          
- <h3 className='mt-5 pt-5'>Enter Your Text Below</h3>
-<div className="mb-3">
-  <textarea  className={` form-control bg-${props.darkMode === 'dark' ? 'black' : 'white'} text-${props.darkMode === 'dark' ? 'white' : 'black'} `}  value={text}  onChange={handleOnChange} id="myBox" rows="7"  ></textarea>
-  
-</div>
-<button disabled={text.length===0} className="btn btn-success " onClick={handleUpClick}>Change into Uppercase</button>
-<button disabled={text.length===0} className="btn btn-primary ms-2 mt-1 " onClick={lowerCase}>Change into Lowercase</button>
-<button disabled={text.length===0} className="btn btn-warning ms-2 mt-1" onClick={nks}>Remove Extra Space</button>
-<button disabled={text.length===0} className="btn btn-danger ms-2 mt-1" onClick={emt}>Clear Text</button>
-</div>
-   <div className="container">
-    <h3 className='mt-3'>Text Summary</h3>
-    {/* <p>Word Count = {text.split(" ").length}</p> */}
-    <p>Word Count = {text.trim().split(/\s+/).filter(Boolean).length}</p>
-    <p>Total Characters = {text.trim().split(/\s+/).filter(Boolean).join("").length}</p>
-    <h3>Preview-</h3>
-    <p id="koko" >" {text} "</p>
-   </div>
- 
-
-
-</div>
-  )
+    <div className={`bg-${props.darkMode} text-${props.darkMode === 'dark' ? 'white' : 'dark'} rounded pr-3 pb-3 pl-3`}>
+      <div className="container">
+        <h3 className='mt-5 pt-5'>Enter Your Text Below</h3>
+        <div className="mb-3">
+          <textarea
+            className={`form-control bg-${props.darkMode === 'dark' ? 'black' : 'white'} text-${props.darkMode === 'dark' ? 'white' : 'black'}`}
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="7"
+          ></textarea>
+        </div>
+        <button disabled={!text} className="btn btn-success" onClick={handleUpClick}>Change into Uppercase</button>
+        <button disabled={!text} className="btn btn-primary ms-2 mt-1" onClick={lowerCase}>Change into Lowercase</button>
+        <button disabled={!text} className="btn btn-warning ms-2 mt-1" onClick={nks}>Remove Extra Space</button>
+        <button disabled={!text} className="btn btn-danger ms-2 mt-1" onClick={emt}>Clear Text</button>
+      </div>
+      <div className="container">
+        <h3 className='mt-3'>Text Summary</h3>
+        <p>Word Count = {sanitizedText.trim().split(/\s+/).filter(Boolean).length}</p>
+        <p>Total Characters = {sanitizedText.trim().split(/\s+/).filter(Boolean).join("").length}</p>
+        <h3>Preview-</h3>
+        <p id="koko">" {sanitizedText}"</p>
+      </div>
+    </div>
+  );
 }
